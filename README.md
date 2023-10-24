a9fb970c2b2d0e10717be709ebc6117ade9a979c
Iulianm05 <marin523@yahoo.com>
2023-10-24 17:48:35 +0300

    homework 1

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # IntroductionToRobotics
 
 This is my repository that will contain all the assignments from the **"Introduction to Robotics"** laboratory at the **Faculty of Mathematics and Computer Science**, **University of Bucharest**. Each assignment will include detailed documentation, with images and videos.
+
+# Homeworks
+
+## Homework 1
+
+This assignment focuses on controlling an RGB LED using three potentiometers (one for each color).
+
+
+
+For the implementation, I used:
+
+* An RGB LED
+* 3 potentiometers
+* 6 resistors of 100 ohms (I connected three in series for each because I only had one 330-ohm resistor)
+* 1 resistor of 330 ohms
+
+Photo with first homework:
+
+![Tema 1](./Imagini/tema1.jpg)
+
+
+
+This is the video with first homework: https://www.youtube.com/watch?v=EDOw4xzkUVw&ab_channel=IulianMarin
--- Tema2_RGB_LED/Tema2_RGB_LED.ino
+++ Tema2_RGB_LED/Tema2_RGB_LED.ino
@@ -1,3 +1,4 @@
+//Declare constants for the input and output pins
 const int blueSignalPin = 9;
 const int greenSignalPin = 10;
 const int redSignalPin = 11;
@@ -5,11 +6,13 @@ const int analogBlueSignalPin = A0;
 const int analogGreenSignalPin = A1;
 const int analogRedSignalPin = A2;
 
+//Constants for the map parameters
 const int maxPotSignal = 1023;
 const int minPotSignal = 0;
 const int maxAnalogSignal = 255;
 const int minAnalogSignal = 0;
 
+//Variables for signal of potentiometers(0-1023) and signal of led (0-254)
 int redSignalPot = 0;
 int greenSignalPot = 0;
 int blueSignalPot = 0;
@@ -18,7 +21,7 @@ int greenSignalLed = 0;
 int blueSignalLed = 0;
 
 void setup() {
-
+  // Define the output and input pins
   pinMode(redSignalPin,OUTPUT);
   pinMode(greenSignalPin,OUTPUT);
   pinMode(blueSignalPin, OUTPUT);
@@ -29,15 +32,19 @@ void setup() {
 }
 
 void loop() {
+
+  //Read the signal transmitted from the potentiometer to the input pins.
   redSignalPot = analogRead(analogRedSignalPin);
   greenSignalPot = analogRead(analogGreenSignalPin) ;
   blueSignalPot = analogRead(analogBlueSignalPin);
+
+  //Map the signal
   redSignalLed = map(redSignalPot,minPotSignal,maxPotSignal,minAnalogSignal,maxAnalogSignal);
   greenSignalLed = map(greenSignalPot,minPotSignal,maxPotSignal,minAnalogSignal,maxAnalogSignal);
   blueSignalLed = map(blueSignalPot,minPotSignal,maxPotSignal,minAnalogSignal,maxAnalogSignal);
+  
+  //Write the signal to the corresponding LED pin
   analogWrite(redSignalPin,redSignalLed);
   analogWrite(greenSignalPin,greenSignalLed);
   analogWrite(blueSignalPin,blueSignalLed);
-
-
 }
